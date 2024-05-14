c05cb96978c44ca542424ec85b7b17e7d685e447
Chitra Kabirdoss <64376287+chitragans@users.noreply.github.com>
2024-05-14 00:07:05 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,61 +1,18 @@
 import streamlit as st
-from langchain import PromptTemplate
 from langchain.llms import OpenAI
 
-template = """
-    Below is an email that may be poorly worded.
-    Your goal is to:
-    - Properly format the email
+st.title('🦜🔗 Quickstart App')
 
-    Please start the email with a warm introduction. Add the introduction if you need to.
-    
-    Below is the email:
-    EMAIL: {email}
-    
-    RESPONSE:
-"""
+openai_api_key = st.sidebar.text_input('OpenAI API Key')
 
-prompt = PromptTemplate(
-    input_variables=["email"],
-    template=template,
-)
+def generate_response(input_text):
+  llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+  st.info(llm(input_text))
 
-def load_LLM(openai_api_key):
-    llm = OpenAI(temperature=.7, openai_api_key=openai_api_key)
-    return llm
-
-st.set_page_config(page_title="Convert Text to Email")
-st.markdown("## Enter Your Email To Convert")
-
-def get_api_key():
-    #input_text = st.text_input(label="OpenAI API Key ",  placeholder="Ex: sk-2twmA8tfCb8un4...", key="openai_api_key_input")
-    input_text = st.text_input(label="OpenAI API Key ",  type="password", key="openai_api_key_input")
-    return input_text
-
-openai_api_key = get_api_key()
-
-def get_text():
-    input_text = st.text_area(label="Type here", placeholder="Your Email...", key="email_input")
-    return input_text
-
-email_input = get_text()
-
-if len(email_input.split(" ")) > 700:
-    st.write("Please enter a shorter email up to a max of 700 words")
-    st.stop()
-
-st.markdown("### Here is your Drafted Email:")
-
-if email_input:
-    if not openai_api_key:
-        st.warning('Enter your OpenAI API Key.)', icon="🔥")
-        st.stop()
-
-    llm = load_LLM(openai_api_key=openai_api_key)
-
-    #prompt_with_email = prompt.format(tone=option_tone, dialect=option_dialect, email=email_input)
-    prompt_with_email = prompt.format(email=email_input)
-
-    formatted_email = llm(prompt_with_email)
-
-    st.write(formatted_email)
+with st.form('my_form'):
+  text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
+  submitted = st.form_submit_button('Submit')
+  if not openai_api_key.startswith('sk-'):
+    st.warning('Please enter your OpenAI API key!', icon='⚠')
+  if submitted and openai_api_key.startswith('sk-'):
+    generate_response(text)
